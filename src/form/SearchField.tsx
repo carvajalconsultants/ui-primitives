956f004e790d54358a955e0b2bb082e296ba7feb
Khaan25 <ziarzai@hotmail.com>
2025-04-18 17:25:04 +0500

    feat: search field styles

--- src/form/SearchField.tsx
+++ src/form/SearchField.tsx
@@ -14,12 +14,15 @@ import type { InputVariantProps } from "../../styled-system/recipes";
 
 type SearchFieldProps = Omit<InputProps, "size" | "onChange"> &
   Partial<InputVariantProps> & {
+    // If we want to pass some additional styles
+    className?: string;
+
     onClear?: () => void;
     debounceTime?: number;
     onDebounceChange?: (value: string | undefined) => void;
   };
 
-export const SearchField = ({ onClear, size, debounceTime = 500, value: initialValue = "", onDebounceChange, bordered = false, ...props }: SearchFieldProps) => {
+export const SearchField = ({ onClear, size, debounceTime = 500, value: initialValue = "", onDebounceChange, bordered = false, className, ...props }: SearchFieldProps) => {
   const classes = input({ size, bordered });
 
   // Handle input changes
@@ -63,7 +66,8 @@ export const SearchField = ({ onClear, size, debounceTime = 500, value: initialV
             classes.input,
             css({
               paddingLeft: size === "sm" ? "9" : "10",
-            })
+            }),
+            className
           )}
           {...props}
         />
