265aea42cce2cf4f0626711df2663022ddaaef2a
Khaan25 <ziarzai@hotmail.com>
2025-05-12 14:02:01 +0500

    feat: done

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,6 +1,8 @@
 import { Button as AriaButton } from "react-aria-components";
 
+import { Box } from "../../styled-system/jsx";
 import { button } from "../../styled-system/recipes/button";
+import { Spinner } from "../common/Spinner";
 
 import type { FC } from "react";
 import type { AriaButtonProps } from "react-aria";
@@ -8,9 +10,31 @@ import type { AriaButtonProps } from "react-aria";
 import type { ButtonVariantProps } from "../../styled-system/recipes";
 import type { WithoutClassName } from "../types";
 
-export type ButtonProps = WithoutClassName<AriaButtonProps> & Partial<ButtonVariantProps>;
+export type ButtonProps = WithoutClassName<AriaButtonProps> &
+  Partial<ButtonVariantProps> & {
+    /**
+     * If true, shows a loading spinner and disables the button.
+     */
+    isLoading?: boolean;
+  };
 
 /**
  * ARIA compliant button that calls onPress function when clicked.
  */
-export const Button: FC<ButtonProps> = ({ variant, width, size, ...props }) => <AriaButton className={button({ variant, width, size })} {...props} />;
+export const Button: FC<ButtonProps> = ({ variant = "primary", width, size, isLoading = false, isDisabled, children, ...props }) => {
+  // If loading, always disable the button
+  const disabled = isDisabled ?? isLoading;
+
+  return (
+    <AriaButton className={button({ variant, width, size })} isDisabled={disabled} aria-disabled={disabled} {...props}>
+      {isLoading ? (
+        <Box display="flex" alignItems="center" justifyContent="center" gap="2">
+          <Spinner size="4" data-spinner />
+          {children}
+        </Box>
+      ) : (
+        children
+      )}
+    </AriaButton>
+  );
+};
