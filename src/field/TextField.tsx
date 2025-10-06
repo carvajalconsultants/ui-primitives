ff1ec15c51a1b53b0035d36828db795e1d2d8be5
Khaan25 <ziarzai@hotmail.com>
2025-10-06 20:11:56 +0500

    feat: text field

--- src/field/TextField.tsx
+++ src/field/TextField.tsx
@@ -1,10 +1,10 @@
+import { forwardRef } from "react";
 import { TextField as AriaTextField, FieldError, Input } from "react-aria-components";
 
 import { input } from "../../styled-system/recipes";
 import { Label } from "../common/Label";
 import { Text } from "../typography/Text";
 
-import type { FC } from "react";
 import type { TextFieldProps as AriaTextFieldProps } from "react-aria-components";
 
 import type { InputVariantProps } from "../../styled-system/recipes";
@@ -25,16 +25,18 @@ export type TextFieldProps = WithoutClassName<AriaTextFieldProps> &
     description?: string;
   };
 
-export const TextField: FC<TextFieldProps> = ({ size, label, placeholder, description, bordered = true, ...props }) => {
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(({ size, label, placeholder, description, bordered = true, ...props }, ref) => {
   const classes = input({ size, bordered });
 
   return (
     <AriaTextField className={classes.wrapper} {...props}>
       <Label color="primary">{label}</Label>
-      <Input className={classes.input} placeholder={placeholder} />
+      <Input ref={ref} className={classes.input} placeholder={placeholder} />
 
       <FieldError className={classes.error} />
       {description && <Text slot="description">{description}</Text>}
     </AriaTextField>
   );
-};
+});
+
+TextField.displayName = "TextField";
