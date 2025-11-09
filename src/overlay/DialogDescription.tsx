30e0d0aa80b9779e173a690eead0be1d9284c313
Khaan25 <ziarzai@hotmail.com>
2025-11-09 15:46:15 +0500

    feat: refactor Dialog components to use children prop for titles and descriptions

--- src/overlay/DialogDescription.tsx
+++ src/overlay/DialogDescription.tsx
@@ -1,15 +1,14 @@
 import { Paragraph } from "../typography/Paragraph";
 
-import type { FC } from "react";
+import type { FC, PropsWithChildren } from "react";
 
-export interface DialogDescriptionProps {
-  /**
-   * Description displayed below the title in the dialog header.
-   */
-  description: string;
-}
+import type { TextVariant } from "../../styled-system/recipes";
+
+type ParagraphProps = PropsWithChildren & Partial<TextVariant>;
+
+export type DialogDescriptionProps = ParagraphProps;
 
 /**
  * Dialog description component displayed below the title in the dialog header.
  */
-export const DialogDescription: FC<DialogDescriptionProps> = ({ description }) => <Paragraph>{description}</Paragraph>;
+export const DialogDescription: FC<DialogDescriptionProps> = ({ children, ...props }) => <Paragraph {...props}>{children}</Paragraph>;
