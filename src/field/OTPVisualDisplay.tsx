6f3eda58c577841510a0c80eede4dc6787a818c6
Khaan25 <ziarzai@hotmail.com>
2025-12-02 18:45:38 +0500

    chore: add separator comment in OTPVisualDisplay component

--- src/field/OTPVisualDisplay.tsx
+++ src/field/OTPVisualDisplay.tsx
@@ -48,6 +48,7 @@ export const OTPVisualDisplay: FC<OTPVisualDisplayProps> = ({ focused }) => {
         </div>
       </div>
 
+      {/* A separator between the first and second group of digits */}
       <div role="separator" className={styles.separator}>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
           <path d="M5 12h14"></path>
